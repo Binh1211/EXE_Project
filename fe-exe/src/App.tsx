f336cb9cb136695b9f2e5897a39f080c84556bf2
Binh1211 <hoangcongbinh121104@gmail.com>
2026-02-26 09:44:24 +0700

    Initial commit

---
+++ fe-exe/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="bg-yellow-500 text-white p-10 text-3xl">
+        Tailwind đang hoạt động 🚀
+      </Button>
+    </>
+  );
+}
+
+export default App;
