92d0f803f062a1bc5039b2f0a071a3e035379893
TuanAnh164 <xmy283480@gmail.com>
2026-02-26 14:33:02 +0700

    add header

--- fe-exe/src/App.tsx
+++ fe-exe/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import { Button } from "./components/ui/button";
-
+import Header from "./components/layouts/landing/header";
 function App() {
   return (
     <>
-      <Button className="bg-yellow-500 text-white p-10 text-3xl">
-        Tailwind đang hoạt động 🚀
-      </Button>
+      <Header />
+
     </>
   );
 }
